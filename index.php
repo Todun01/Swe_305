88121a2ed109a5ec989dad01250f015dcf71e10c
Todun01 <obafemitodunni@gmail.com>
2023-10-18 01:59:35 -1100

    update

--- index.php
+++ index.php
@@ -1,7 +1,31 @@
 <?php
     $name = 'oTAG0';
+    $age = 18;
+    $cgpa = 4.78;
+    $male = true;
+    $courses = ["swe301", "swe311", "swe309"];
+    var_dump($age);
+    /*
+    foreach ($courses as $course) {
+        $favoriteCourse = $course = "swe307" ? $course :null;
+    }
+    */
+    $favoriteCourse = array_filter($courses, "sta343")? "sta343": null;
+    $myCourse = "mth309";
+/*
+    class Users{
+        function user(){
+            $this->name = "Todun";
+        }
+    }
+*/
    // echo $name;
+   function sum($x, $y){
+    return $x + $y;
+   }
+   $addition = sum(10, 8);
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,6 +35,16 @@
 </head>
 <body>
     <h1 style="
-    color: red; background-color: black; padding: 20px"> Welcome! <?= $name ?></h1>
+    color: red; background-color: black; padding: 20px"> Welcome! <?= $name ?>. You are <?= $addition ?></h1>
+    <p> You offer the following courses </p>
+    <ul>
+        <?php foreach ($courses as $course){ ?>
+        <li><?= $course ?></li>
+        <?php if($course == 'swe309'){ ?>
+          Your favorite course is <?=  $favoriteCourse = "swe309"; ?>
+        <?php } ?>
+        <?php } ?>
+    </ul>
 </body>
 </html>
+
