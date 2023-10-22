ca2b8405e7a1f943b820a12e6107f83739a8782a
Todun01 <obafemitodunni@gmail.com>
2023-10-22 11:29:14 -1100

    updating records in a databas

--- edit.php
+++ edit.php
@@ -1,7 +1,35 @@
 <?php
     include("connection.php");
 
-    $userId = $_GET["user_id"];
+    if(isset($_POST["update_entry"])){
+        $legalname = mysqli_escape_string($connection, $_POST["legalname"]);
+        $username = mysqli_escape_string($connection, $_POST["username"]);
+        $os = mysqli_escape_string($connection, $_POST["os"]);
+        $favgamegenre = mysqli_escape_string($connection, $_POST["favorite_game_genre"]);
+        $email = mysqli_escape_string($connection, $_POST["email"]);
+        $password = mysqli_escape_string($connection, $_POST["password"]);
+        $confirmpassword = mysqli_escape_string($connection, $_POST["confirmpassword"]);
+        $userId = mysqli_escape_string($connection, $_POST["id"]);
+    
+        $error = null;
+        $success = null;
+        $pass = null;
+    
+        if($password != $confirmpassword){
+            $error = "Your passwords are not the same!";
+        }else{
+            $pass = md5($password);
+        }
+        $updateQuery = "UPDATE customers SET legalname = '$legalname', username = '$username', os = '$os', favorite_game_genre='$favgamegenre', email = '$email', password = '$pass' WHERE id = '$userId'";
+        $saveQuery = mysqli_query($connection, $updateQuery);
+        if($saveQuery){
+            $success = "Changes saved Successfully";
+        }else{
+            $error = "Oops! Failed to update record";
+        }
+    }
+
+    $userId = isset($_POST['id'])?$_POST['id']:$_GET['id'];
     $fetchUserQuery = "SELECT * FROM customers WHERE id = $userId";
     $fetchUserResult = mysqli_query($connection, $fetchUserQuery);
     $fetchUserData = mysqli_fetch_assoc($fetchUserResult);
@@ -12,10 +40,40 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Edit</title>
+    <style>
+        .error{
+            background-color: red;
+            color: black;
+            padding: 30px;
+        }
+        .success{
+            background-color: green;
+            color: black;
+            padding: 30px;
+        }
+        body{
+            background-color: black;
+        }
+        p{
+            color: white;
+
+        }
+        h1{
+            color: black;
+            background-color: darkred;
+            padding: 25px;
+        }
+    </style>
 </head>
 <body>
+    <?php if(!empty($error)){ ?>
+        <p class="error"><?= $error ?></p>
+    <?php } ?>
+    <?php if(!empty($success)){ ?>
+        <p class="success"><?= $success ?></p>
+    <?php } ?>
     <?php if(!empty($fetchUserData)) { ?>
-<form action="assignment2.php" method="POST">
+<form action="edit.php" method="POST">
         <p>
             Legal Name: <input type="text" name="legalname" id="" value=<?= $fetchUserData['legalname']?>>
         </p>
@@ -32,9 +90,15 @@
             Email: <input type="email" name="email" id="" value=<?= $fetchUserData['email']?>>
         </p>
         <p>
-            Password: <input type="password" name="password" id="" value=<?= $fetchUserData['password']?>>
+            Password: <input type="password" name="password" id="" >
         </p>
-        <button type="submit" name="add_entry">I'm Ready!</button>
+        <p>
+            Confirm Password: <input type="password" name="confirmpassword" id="">
+        </p>
+        <p>
+            <input type="hidden" name="id" value="<?= $fetchUserData['id']?>">
+        </p>
+        <button type="submit" name="update_entry">Save Changes</button>
     </form>
     <?php } else{ ?>
         <h1>User not found</h1>
